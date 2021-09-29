f6fbc726ad5c52f28197e7a1e1f27472ec62787b
sonjafiliposka <58505801+sonjafiliposka@users.noreply.github.com>
2021-09-29 12:17:07 +0200

    Add files via upload

---
+++ CloudSim/runAll-OmnetInput.sh
@@ -0,0 +1,132 @@
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 5074 initialPositioning-4951.txt migrations-4951.txt
+
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 5013 initialPositioning-4955.txt migrations-4955.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 4256 initialPositioning-4956.txt migrations-4956.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 5808 initialPositioning-5734.txt migrations-5734.txt
+
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 5858 initialPositioning-5749.txt migrations-5749.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 4966 initialPositioning-5740.txt migrations-5740.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+#  java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+#  java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+#  java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 6187 initialPositioning-6915.txt migrations-6915.txt
+
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+#  java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+#  java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 7004 initialPositioning-6923.txt migrations-6923.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 6042 initialPositioning-6910.txt migrations-6910.txt
+# 
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 7455 initialPositioning-8600.txt migrations-8600.txt
+# 
+# java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 7463 initialPositioning-8620.txt migrations-8620.txt
+# 
+java -jar 1s_CloudSimSUMORun.jar 45 1 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 45 0 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 63 1 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 63 0 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 81 1 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 81 0 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 99 1 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 99 0 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 117 1 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
+java -jar 1s_CloudSimSUMORun.jar 117 0 sc sc-mad 7486 initialPositioning-8635.txt migrations-8635.txt
